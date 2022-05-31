c1dbef944b94700534b4dc4626b4e003e394af33
AnastasiyaMAV <mihalukova112@mail.ru>
2022-05-31 15:24:49 +0200

    add: PriceList components

--- src/components/Pages/Price/PriceList/PriceList.jsx
+++ src/components/Pages/Price/PriceList/PriceList.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import './PriceList.scss';
+import {
+  titleTable, nameTable, concreteMix, concreteSolution,
+} from '../../../../utils/price_db';
 
 function PriceList() {
   return (
-    <section>
-      <p>PriceList</p>
+    <section className="container-priceList">
+      <h1>Прайс лист</h1>
+      <table>
+        <tr>
+          <td>{titleTable.nameproduct}</td>
+          <td>{titleTable.properties}</td>
+          <td>{titleTable.price}</td>
+          <td>{titleTable.VAT}</td>
+          <td>{titleTable.cash}</td>
+        </tr>
+        <tr>
+          <td>{nameTable.concreteMixName}</td>
+        </tr>
+        {
+          concreteMix.map((item) => (
+            <tr key={item.id}>
+              <td>{item.nameproduct}</td>
+              <td>{item.properties}</td>
+              <td>{item.price}</td>
+              <td>{item.VAT}</td>
+              <td>{item.cash}</td>
+            </tr>
+          ))
+        }
+        <tr>
+          <td>{nameTable.concreteSolutionName}</td>
+        </tr>
+        {
+          concreteSolution.map((item) => (
+            <tr key={item.id}>
+              <td>{item.nameproduct}</td>
+              <td>{item.properties}</td>
+              <td>{item.price}</td>
+              <td>{item.VAT}</td>
+              <td>{item.cash}</td>
+            </tr>
+          ))
+        }
+      </table>
     </section>
   );
 }
