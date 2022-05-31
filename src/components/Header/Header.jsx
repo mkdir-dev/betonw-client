ecf7bb098414cc399da35fcee37510f05ccac488
Mikhail Koryukov <mezhovkin@yandex.ru>
2022-05-31 13:35:22 +0300

    feat: add fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './Header.scss';
+
 function Header() {
   return (
-    <header>
-      <p>header</p>
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo-wrap">
+          <div className="header__logo" />
+          <p>Beton-W</p>
+        </div>
+      </div>
     </header>
   );
 }
