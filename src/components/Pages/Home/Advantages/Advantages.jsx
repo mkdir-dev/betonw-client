c5788e0688529905c8f8bcadcc438ffa14108823
Mikhail Koryukov <mezhovkin@yandex.ru>
2022-06-01 13:04:14 +0300

    feat: add advantages

--- src/components/Pages/Home/Advantages/Advantages.jsx
+++ src/components/Pages/Home/Advantages/Advantages.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import './Advantages.scss';
+
+import { advantages } from '../../../../utils/constants';
+
 function Advantages() {
+  // console.log(advantages);
+
   return (
     <section className="advantages">
-      <p>Advantages</p>
+      <h2 className="advantages__title">Наши преимущества</h2>
+      <ul className="advantages__list">
+        {advantages.map((advant) => (
+          <ol
+            key={advant.id}
+            className="advantages__item"
+          >
+            <span className="advantages__item-num">
+              {advant.id}
+            </span>
+            <p className="advantages__item-text">
+              {advant.text}
+            </p>
+          </ol>
+        ))}
+      </ul>
     </section>
   );
 }
